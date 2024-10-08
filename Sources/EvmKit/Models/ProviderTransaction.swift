bfb11081287b830fded43145b49a3357e098f052
Dmitriy Mikitenko <>
2024-10-08 12:39:44 +0300

    fix parcer

--- Sources/EvmKit/Models/ProviderTransaction.swift
+++ Sources/EvmKit/Models/ProviderTransaction.swift
@@ -21,21 +21,21 @@ public struct ProviderTransaction: ImmutableMappable {
     let gasUsed: Int?
 
     public init(map: Map) throws {
-        blockNumber = try map.value("blockNumber", using: StringIntTransform())
-        timestamp = try map.value("timeStamp", using: StringIntTransform())
-        hash = try map.value("hash", using: HexDataTransform())
-        nonce = try map.value("nonce", using: StringIntTransform())
-        blockHash = try? map.value("blockHash", using: HexDataTransform())
-        transactionIndex = try map.value("transactionIndex", using: StringIntTransform())
-        from = try map.value("from", using: HexAddressTransform())
-        to = try? map.value("to", using: HexAddressTransform())
-        value = try map.value("value", using: StringBigUIntTransform())
-        gasLimit = try map.value("gas", using: StringIntTransform())
-        gasPrice = try map.value("gasPrice", using: StringIntTransform())
-        isError = try? map.value("isError", using: StringIntTransform())
-        txReceiptStatus = try? map.value("txreceipt_status", using: StringIntTransform())
-        input = try map.value("input", using: HexDataTransform())
-        cumulativeGasUsed = try? map.value("cumulativeGasUsed", using: StringIntTransform())
-        gasUsed = try? map.value("gasUsed", using: StringIntTransform())
+        blockNumber = try map.value("blockNumber", using: StringIntTransform()) as Int
+        timestamp = try map.value("timeStamp", using: StringIntTransform()) as Int
+        hash = try map.value("hash", using: HexDataTransform()) as Data
+        nonce = try map.value("nonce", using: StringIntTransform()) as Int
+        blockHash = try? map.value("blockHash", using: HexDataTransform()) as Data?
+        transactionIndex = try map.value("transactionIndex", using: StringIntTransform()) as Int
+        from = try map.value("from", using: HexAddressTransform()) as Address
+        to = try? map.value("to", using: HexAddressTransform()) as Address?
+        value = try map.value("value", using: StringBigUIntTransform()) as BigUInt
+        gasLimit = try map.value("gas", using: StringIntTransform()) as Int
+        gasPrice = try map.value("gasPrice", using: StringIntTransform()) as Int
+        isError = try? map.value("isError", using: StringIntTransform()) as Int?
+        txReceiptStatus = try? map.value("txreceipt_status", using: StringIntTransform()) as Int?
+        input = try map.value("input", using: HexDataTransform()) as Data
+        cumulativeGasUsed = try? map.value("cumulativeGasUsed", using: StringIntTransform()) as Int?
+        gasUsed = try? map.value("gasUsed", using: StringIntTransform()) as Int?
     }
 }
